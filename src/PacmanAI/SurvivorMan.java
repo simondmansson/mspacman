fdd860afdb0f9778497bffff0e94f46df5827ec2
Kalk <simon.d.mansson@gmail.com>
2017-10-17 11:41:56 +0200

    Framework

---
+++ src/PacmanAI/SurvivorMan.java
@@ -0,0 +1,18 @@
+package PacmanAI;
+import static pacman.game.Constants.*;
+import pacman.controllers.Controller;
+import pacman.game.*;
+
+/**
+ * @author Kalk88 & Kemaldev
+ * The survivorMan lives for one purpose only... Never die.
+ *
+ */
+public class SurvivorMan extends Controller<MOVE>{
+
+    @Override
+
+    public MOVE getMove(Game game, long timeDue) {
+        return null;
+    }
+}
