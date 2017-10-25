1094d42f130af8ea76a5fa0d147c080741d4e592
Kalk <simon.d.mansson@gmail.com>
2017-10-25 11:41:43 +0200

    Work in integrating Graphvisualiser

--- src/test/java/TreePrinterTests.java
+++ src/test/java/TreePrinterTests.java
@@ -1,9 +1,13 @@
 import PacmanAI.TreeNode;
+import org.graphstream.graph.Graph;
+import org.graphstream.graph.Node;
+import org.graphstream.graph.implementations.SingleGraph;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Random;
 
 public class TreePrinterTests {
     private TreeNode root;
@@ -19,7 +23,7 @@ public class TreePrinterTests {
         left.addEdge("true", leftleft);
         left.addEdge("false", leftright);
         TreeNode right = new TreeNode("inkyIsEdible");
-        right.addEdge("inkyDist", new TreeNode("RIGHT"));
+        right.addEdge("inkyDist", new TreeNode("DOWN"));
         root.addEdge(left.getLabel(), left);
         root.addEdge(right.getLabel(), right);
         root.addEdge("inkyDir", new TreeNode("UP"));
@@ -65,6 +69,48 @@ public class TreePrinterTests {
             if(!node.isLeaf())
                 System.out.println();
         }
+    }
+
+    @Test
+    public void GraphStreamTest() {
+        Graph graph = new SingleGraph("Tutorial 1");
+
+        graph.addNode("A");
+        graph.addNode("B");
+        graph.addNode("C");
+        graph.addEdge("AB", "A", "B");
+        graph.addEdge("BC", "B", "C");
+        graph.addEdge("CA", "C", "A");
+
+        graph.display();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+    @Test
+
+    public void GraphStreamTest2() {
+        Graph graph = new SingleGraph("TestTree");
+        TreeNode left = new TreeNode("blinkyIsEdible");
+        TreeNode leftleft = new TreeNode("true");
+        TreeNode leftright = new TreeNode("false");
+        TreeNode[] vertices = { root, left, leftleft, leftright};
+        for(int i = 0; i < vertices.length; i++) {
+            Node n = graph.addNode(vertices[i].toString());
+            n.addAttribute("ui.label", vertices[i].toString());
+        }
+        graph.addEdge(root.toString()+left.toString(), root.toString(), left.toString());
+        graph.addEdge(left.toString()+leftleft.toString(), left.toString(), leftleft.toString());
+        graph.addEdge(left.toString()+leftright.toString(), left.toString(), leftright.toString());
+        graph.display();
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
     }
 
     private void print(LinkedList<TreeNode> nodes, int offset) {
